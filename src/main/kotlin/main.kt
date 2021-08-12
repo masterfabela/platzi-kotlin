93ad7392df380b29f7ca8928298fae5202d1a9b9
Jose Ramon Vieites Romay <joseviromay@gmail.com>
2021-08-12 16:22:31 +0200

    Clase 9

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -6,7 +6,22 @@ fun main() {
     println(money)
     money = 5
     println(money)
-    val name = "Maria"
+    var name = "Maria"
     println(name)
     println(PI)
+
+    val boolean = true
+    val longNumber = 30000000000000000L
+    val double = 2.3423
+    val float = 1.1f
+
+    val firsValue = 20
+    val secondValue = 10
+    val thirdValue = firsValue - secondValue
+    println(thirdValue)
+
+    val surname = "Vieites"
+    name = "Jose"
+    val completeName = "Mi nombre es $name $surname"
+    println(completeName)
 }
